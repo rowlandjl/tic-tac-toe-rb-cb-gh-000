b0eb78e459b06a0e5826923e6d5a8ba1098236ec
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 04:20:34 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -42,7 +42,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
 
-  if valid_move?(board, position) == true
+  if valid_move?(board, position)
     move(board, index)
     display_board(board)
   else
