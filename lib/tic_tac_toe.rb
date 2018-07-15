900dfaccc81867d62d07aad6030606e00539d262
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 03:57:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,3 +54,14 @@ def turn(board)
     input = gets.strip
   end
 end
+
+def won?(board)
+  WIN_COMBINATIONS.each_with_index do |element, index|
+    if position_taken?(board, element[0]) == true
+      if board[element[0]] == board[element[1]] && board[element[1]] == board[element[2]]
+        return WIN_COMBINATIONS[index]
+      end
+    end
+  end
+  return false
+end
