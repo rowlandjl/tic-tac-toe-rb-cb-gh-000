5590295ddadb69e64e98295d750b755dddf261e9
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 04:18:28 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,11 +34,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index - 1) || board[index] == nil
-    false
-  else
-    true
-  end
+  !position_taken?(board, index.to_i - 1) && (1..9).include?(index.to_i)
 end
 
 def turn(board)
