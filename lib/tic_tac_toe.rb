60fe818995e484aed01d01807f538308c832407c
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-15 04:21:02 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,7 +43,7 @@ def turn(board)
   index = input_to_index(input)
 
   if valid_move?(board, position)
-    move(board, index)
+    move(board, index, current_player(board))
     display_board(board)
   else
     puts "Please enter 1-9:"
